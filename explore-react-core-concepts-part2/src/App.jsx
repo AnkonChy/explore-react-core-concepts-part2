ee68dcf220fc996905d1940076326314c11463e4
Ankon Chowdhury <ankonchy1@gmail.com>
2024-10-20 17:43:01 +0600

    explore event, state, use effect

---
+++ explore-react-core-concepts-part2/src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import Counter from "./Counter";
+import Friends from "./Friends";
+import Team from "./Team";
+import Users from "./Users";
+
+function App() {
+  function handleClick() {
+    alert("button click");
+  }
+
+  const handleClick2 = () => {
+    alert("button 2 click");
+  };
+  const addToFive = (num) => {
+    alert(num + 5);
+  };
+
+  return (
+    <>
+      <h3>React Core Concepts 2</h3>
+
+      <Friends></Friends>
+      <Users></Users>
+
+      <Team></Team>
+      <Counter></Counter>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click Me 2</button>
+      <button onClick={() => alert("Third btn clicked")}>Third</button>
+
+      {/* vejailla  */}
+      <button onClick={() => addToFive(3)}>Four</button>
+    </>
+  );
+}
+
+export default App;
